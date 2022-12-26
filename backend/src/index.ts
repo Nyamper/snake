69ad2217fd6f8889c753443f4c097aee1405af78
Nyamper <campersf@gmail.com>
2022-12-26 18:53:05 +0200

    cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import router from './routes';
+import cors from 'cors';
 
 const { PORT, MONGO_URI } = process.env;
 
 const app = express();
 
+app.use(cors);
+
 app.use(express.urlencoded());
 app.use(express.json());
+
+const allowedOrigins = ['*'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
 
 app.use('/api', router);
 
